25c87fb52376a6f2d02a51b2f6abc26d8a43076c
Narub-Iqbal <95308975+Narub-Iqbal@users.noreply.github.com>
2022-01-14 05:11:59 -0800

    abubakkar files for test dev

---
+++ bakkar_file.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x;
+	int y;
+	cout<<"First value \n";
+cin>>x;
+cout<<"second value \n";
+cin>>y;	
+if (x>y)
+{
+	for (int i=1;i<4;i++)
+	{
+		cout<<" "<<x;
+	}
+	
+	
+}
+else
+{
+		for (int z=1;z<4;z++)
+	{
+		cout<<" "<<y;
+	}
+	
+	
+}
+}
